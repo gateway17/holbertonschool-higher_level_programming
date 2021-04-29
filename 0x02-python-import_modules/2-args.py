298bb0711dcca0198f6edb2bba356a79bfc4b45d
Andres Sepulveda <holberton@NNRX2000>
2021-04-28 22:35:40 -0500

    Pep8 warnings fixed.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if len(sys.argv) == 2:
-    print("{} argument:".format(len(sys.argv) - 1))
-    print("1: {}".format(sys.argv[1]))
-elif len(sys.argv) == 1:
-    print("{} arguments.".format(0))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("1: {}".format(sys.argv[1]))
+    elif len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-a = 0
-for i in range(len(sys.argv) - 1):
-    a = int(a) + int(sys.argv[i + 1])
-print(a)
+if __name__ == "__main__":
+    import sys
+    a = 0
+    for i in range(len(sys.argv) - 1):
+        a = int(a) + int(sys.argv[i + 1])
+    print(a)
