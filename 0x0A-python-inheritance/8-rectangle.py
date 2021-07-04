c87d83fb6ed59af3c6afec06e6d9616657e32656
Andres Sepulveda <holberton@NNRX2000>
2021-07-03 21:00:44 -0500

    Fixing bad Checkers.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,10 +13,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
+    """ Set a Rectangle."""
     def __init__(self, width, height):
-
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+        """Initialize values for width and height """
+        if super().integer_validator("width", width):
+            self.__width = width
+        if super().integer_validator("height", height):
+            self.__height = height
