576688686748ac2d669ea1d03afd50a2f2db39a4
Andres Sepulveda <holberton@NNRX2000>
2021-07-03 20:24:22 -0500

    Fixing bad Checkers.

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -11,7 +11,8 @@ Write a class BaseGeometry (based on 5-base_geometry.py).
 
 
 class BaseGeometry:
-    """  """
+    """This class is useless. """
     def area(self):
-        """ """
+        """raises an Exception with
+        the message area() is not implemented """
         raise Exception("area() is not implemented")
