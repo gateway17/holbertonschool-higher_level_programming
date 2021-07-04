7abdb56ef26bce1b364fa979445de8388176834a
Andres Sepulveda <holberton@NNRX2000>
2021-07-03 20:28:05 -0500

    Fixing bad Checkers.

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,13 +17,18 @@ Write a class BaseGeometry (based on 6-base_geometry.py).
 
 
 class BaseGeometry:
-    """	"""
+    """Geometry operations. """
     def area(self):
-        """ """
+        """raises an Exception with
+        the message area() is not implemented """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ """
+        """validates value:
+        if value is not an integer: raise a TypeError exception,
+        with the message <name> must be an integer
+        if value is less or equal to 0: raise a ValueError exception with
+        the message <name> must be greater than 0 """
         if type(value) != int:
             raise(TypeError("<name> must be an integer"))
 
