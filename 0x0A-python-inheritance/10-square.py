249358fd142991a25e5adecef5f3661a2aa66dc8
Andres Sepulveda <holberton@NNRX2000>
2021-07-03 21:37:54 -0500

    Fixing bad Checkers.

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""(inherits from Rectangle) """
+
+
+class Square(Rectangle):
+    """Sets a Square. """
+
+    def __init__(self, size):
+        """Initialize size for Square """
+        self.integer_validator("Value", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns area of Square"""
+        return self.__size**2
