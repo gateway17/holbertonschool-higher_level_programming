bee853226393950151010c8bffc4de2a520775cc
Andres Sepulveda <holberton@NNRX2000>
2021-07-03 19:19:18 -0500

    Fixing checkers.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         elif width < 0:
             raise ValueError('width must be >= 0')
-        elif not isinstance (height, int):
+        elif not isinstance(height, int):
             raise TypeError('height must be an integer')
         elif height < 0:
             raise ValueError('height must be >= 0')
