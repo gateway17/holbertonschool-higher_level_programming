7481e056fa39274fe8804d4e9e06d7272941262a
Andres Sepulveda <holberton@NNRX2000>
2021-07-15 11:19:53 -0500

    Fixed.

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists the number of records with the same score in the table second_table
 -- the number of records for this score with the label number
 
-SELECT score, COUNT(score) AS number FROM second_tableb GROUP BY score ORDER BY score DESC;
+SELECT score AS "score", COUNT(score) AS "number" FROM second_tableb GROUP BY score ORDER BY score DESC;
