c123d725f55c6960685f1c8b570cac71563b1f99
Andres Sepulveda <holberton@NNRX2000>
2021-07-14 19:27:11 -0500

    from 0 to 7th done.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
 -- print the structure (Colums)(Description)
 -- of the "first_table" database
 DESC first_table;
+
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
 -- Counts all records that follow this (id=89)patern.
+
 SELECT COUNT (*) FROM first_table WHERE 'id'=89;
