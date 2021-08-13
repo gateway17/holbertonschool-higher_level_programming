35207412a0928d7184127588631e296122ccc69a
Andres Sepulveda <holberton@NNRX2000>
2021-08-12 23:17:58 -0500

    Fixed./a.out

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -6,6 +6,4 @@ Write an empty class Rectangle that defines a rectangle:
     You must use the class notation for defining your class
 */
 
-class Rectangle {
-
-}
+module.exports = class Rectangle {};
