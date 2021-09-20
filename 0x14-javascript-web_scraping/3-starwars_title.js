248545ed7a14584b178121e88df69dcd655dd521
Andres Sepulveda <holberton@NNRX2000>
2021-09-20 12:30:58 -0500

    Fixing output.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,6 @@ const request = require('request');
 const ID = process.argv[2];
 const api = 'https://swapi-api.hbtn.io/api/films/' + parseInt(ID);
 
-console.log(api);
-
 request(api, function (error, response, body) {
   if (error) throw error;
   console.log(JSON.parse(body).title);
